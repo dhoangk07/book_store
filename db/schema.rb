3d07abcf2b70fb4eaa8794998edb190d6913f4fb
MacOS 10.12 <macos10.12@MacOSs-MBP.net.fpt>
2017-10-20 17:41:15 +0700

    Model Publisher

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020021121) do
+ActiveRecord::Schema.define(version: 20171020103958) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "publishers", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
