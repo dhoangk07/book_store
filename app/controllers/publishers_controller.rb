360a3e049473c635e93140edfb5b1ad41f24828b
MacOS 10.12 <macos10.12@MacOSs-MBP.net.fpt>
2017-10-21 10:19:20 +0700

    Actions for Publisher

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -2,4 +2,33 @@ class PublishersController < ApplicationController
   def index
   	@publishers = Publisher.all
   end
+  
+  def show
+  	# @publisher = Publisher.find(params[:id])
+    @publisher = Publisher.find_by(id: params[:id])
+
+  end
+  
+  def edit
+  	@publisher = Publisher.find(params[:id])
+  end
+  
+  def destroy
+  	@publisher = Publisher.find(params[:id])
+  	@publisher.destroy
+  	redirect_to publishers_path
+  end
+ 
+  def update
+  	@publisher = Publisher.find(params[:id])
+  	if @publisher.update_attributes(publisher_params)
+  		redirect_to publisher_path(@publisher)
+  	else
+  		render :edit
+  	end
+  end
+
+  def publisher_params
+  	params.require(:publisher).permit(:name, :address, :publisher_id)
+  end
 end
