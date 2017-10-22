f7c01973267cca63618808783eb905b5fa7fcb96
MacOS 10.12 <macos10.12@MacOSs-MBP.net.fpt>
2017-10-22 08:38:29 +0700

    input editor data with name Kim Dung, phone, birth_year

--- app/controllers/editors_controller.rb
+++ app/controllers/editors_controller.rb
@@ -1,5 +1,34 @@
 class EditorsController < ApplicationController
 	def index
-  	@editors = Editor.all
+  	  @editors = Editor.all
   	end
+
+  	def show
+  	# @editor = editor.find(params[:id])
+    @editors = Editor.find_by(id: params[:id])
+
+  	end
+  
+  	def edit
+  	@editors = editors.find(params[:id])
+  	end
+  
+  	def destroy
+  	@editors = Editors.find(params[:id])
+  	@editor.destroy
+  	redirect_to editors_path
+  	end
+ 
+  	def update
+  	@editor = Editor.find(params[:id])
+  	if @editor.update_attributes(editor_params)
+  		redirect_to editor_path(@editor)
+  	else
+  		render :edit
+  	end
+
+  	def editor_params
+  	params.require(:editor).permit(:name, :phone, :editor_id)
+  	end
+  end
  end
