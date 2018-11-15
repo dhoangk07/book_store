f37aead1d0b66a448a466c10f5ba9849fc8991fa
HoangDinh <dhoangk07@gmail.com>
2018-11-16 01:15:39 +0700

    Refactor code for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: %i[show edit update destroy]
   def index
   	@users = User.all
   end
 
   def show
-  	@user = User.find(params[:id])
   end
 
   def new
@@ -12,22 +12,18 @@ class UsersController < ApplicationController
   end
 
   def create
-  	# params
   	@user = User.new(user_params)
   	if @user.save
   		redirect_to users_path
   	else
   		render :new
   	end
- 
   end
 
   def edit
-  	@user = User.find(params[:id])
   end
 
   def update
-  	@user = User.find(params[:id])
   	if @user.update_attributes(user_params)
   		redirect_to user_path(@user)
   	else
@@ -36,12 +32,14 @@ class UsersController < ApplicationController
   end
 
   def destroy
-  	@user = User.find(params[:id])
   	@user.destroy
   	redirect_to users_path
   end
 
   private
+  def find_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
   	params.require(:user).permit(:name, :sex, :age, :address)
