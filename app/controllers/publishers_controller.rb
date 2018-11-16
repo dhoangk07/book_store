59f7ccf623c5cb5581a3407ebaaf644b3aeb5bda
HoangDinh <dhoangk07@gmail.com>
2018-11-16 14:22:57 +0700

    Recorrect publisher controller

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -12,6 +12,7 @@ class PublishersController < ApplicationController
   end
 
   def create
+    @publisher = Publisher.new(publisher_params)
   	if @publisher.save
   		redirect_to publishers_path
   	else
@@ -41,6 +42,6 @@ class PublishersController < ApplicationController
   end
 
   def publisher_params
-  	params.require(:publisher).permit(:name, :address, :publisher_id)
+  	params.require(:publisher).permit(:name, :address)
   end
 end
