85aabcd9002a02179782ea90dd367eda5b4d68d4
HoangDinh <dhoangk07@gmail.com>
2018-12-03 20:05:40 +0700

    Add rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,17 @@
 Publisher.all.delete_all
 Category.all.delete_all
 Book.all.delete_all
+Customer.all.delete_all
+Editor.all.delete_all
+Location.all.delete_all
 
-10.times do |n|
+20.times do |n|
   name = Faker::FunnyName.name
   address = Faker::Address.full_address
   Publisher.create(name: name, address: address)
 end
 
-15.times do |n|
+25.times do |n|
   name = Faker::Book.genre
   Category.create(name: name)
 end
@@ -20,7 +23,27 @@ end
   description = Faker::Lorem.paragraph(2)
   price = Faker::Number.decimal(2) 
   Category.all.each do |element|
-        # debugger
     element.books.create!(title: title, author: author, description: description, price: price, publisher_id: Publisher.pluck(:id).sample)
   end
+end
+
+20.times do |n|
+  name = Faker::Name.name    
+  sex = Faker::Gender.binary_type
+  age = (20..40).to_a.sample
+  Customer.create(name: name, sex: sex, age: age)
+end
+
+20.times do |n|
+  name = Faker::Name.name   
+  phone = Faker::PhoneNumber.cell_phone
+  birth_year = (1920..2000).to_a.sample
+  Editor.create(name: name, phone: phone, birth_year: birth_year)
+end
+
+20.times do |n|
+  name = Faker::Address.street_name
+  address = Faker::Address.street_address
+  district = Faker::Address.state
+  Location.create(name: name, address: address, district: district)
 end
