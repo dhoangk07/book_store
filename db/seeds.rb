15620430ea06b3df1f2cbd0942c0e2b91cb8db24
HoangDinh <dhoangk07@gmail.com>
2018-12-04 14:13:14 +0700

    Add User, review, comment for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,9 @@ Book.all.delete_all
 Customer.all.delete_all
 Editor.all.delete_all
 Location.all.delete_all
+User.all.delete_all
+Review.all.delete_all
+Comment.all.delete_all
 
 20.times do |n|
   name = Faker::FunnyName.name
@@ -42,8 +45,30 @@ end
 end
 
 20.times do |n|
-  name = Faker::Address.street_name
+  name = Faker::Address.city
   address = Faker::Address.street_address
+  street = Faker::Address.street_name
   district = Faker::Address.state
-  Location.create(name: name, address: address, district: district)
+  Location.create(name: name, address: address, district: district, street: street)
+end
+
+50.times do |n|
+  name = Faker::Name.name    
+  sex = Faker::Gender.binary_type
+  age = (20..60).to_a.sample
+  address = Faker::Address.street_address
+  email = Faker::Internet.email
+  password = '1234567'
+  password_confirmation = '1234567'
+  User.create(name: name, sex: sex, age: age, address: address, email: email, password: password, password_confirmation: password_confirmation)
+end
+
+50.times do |n|
+  content = Faker::Lorem.paragraph(10)
+  Review.create(content: content, editor_id: Editor.pluck(:id).sample)
+end
+
+50.times do |n|
+  content = Faker::Lorem.paragraph(30)
+  Comment.create(content: content, book_id: Book.pluck(:id).sample)
 end
